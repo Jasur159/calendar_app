ce334fa09c5724b3ec41b3b53469488e58395b9d
Jasur.159 <ganiyevjasur7@gmail.com>
2023-11-17 22:13:34 +0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,77 @@
 const button = document.getElementById("btn");
 let currentTime = new Date();
 let CurrentDate = currentTime.getDate();
-let CurrentMonth = currentTime.getMonth() + 1;
+let CurrentMonth = +currentTime.getMonth() + 1;
 let CurrentYear = currentTime.getFullYear();
 
+let Monthinfo = document.getElementById("month");
+let Yearinfo = document.getElementById("year");
+let Dateinfo = document.getElementById("day");
+let DatePlace = document.getElementById("day_res");
+let MonthPlace = document.getElementById("month_res");
+let YearPlace = document.getElementById("year_res");
+
 button.addEventListener("click", function () {
-  const Dateinfo = +document.getElementById("day").value;
-
-  if (Dateinfo < 32 && Dateinfo > 0) {
-    DayResult = CurrentDate - Dateinfo;
-
-    document.getElementById("day_res").innerHTML = DayResult;
-  } else {
-    alert("Bunday Oy kuni mavjud emas!!!");
-  }
-  const Monthinfo = +document.getElementById("month").value;
-  if (Monthinfo > 0 && Monthinfo < 13) {
-    MonthResult = CurrentMonth - Monthinfo;
-    if (MonthResult == -1) {
-      DayResult += 31;
-    }
-    document.getElementById("month_res").innerHTML = MonthResult;
-  } else {
-    alert("Unday oy mavjud emas");
-  }
-
-  const Yearinfo = +document.getElementById("year").value;
+  calcDateTillNow();
 });
+
+function calcDateTillNow() {
+  let addedMonth = Monthinfo.value;
+  let addedYear = Yearinfo.value;
+  let AddedDate = Dateinfo.value;
+
+  if (!addedMonth || !addedYear || !AddedDate) {
+    alert("Qiymatlarni toliq kiriting");
+    return;
+  }
+  if (addedYear < 0) {
+    alert("Bunday yil mavjud emas !!!!");
+  } else if (addedYear > 2024) {
+    alert("Bu yil hali kelgani yoq");
+    return;
+  }
+  if (addedMonth < 0 && addedMonth > 13) {
+    console.log("BUnday oy mavjud emas");
+    return;
+  }
+  if (AddedDate < 0 && AddedDate > 32) {
+    console.log("BUnday oy kuni mavjud emas");
+  }
+  let yearResult = CurrentYear - addedYear;
+  let monthResult = CurrentMonth - addedMonth;
+  let dateResult = CurrentDate - AddedDate;
+
+  if (monthResult < 0) {
+    monthResult = CurrentMonth + 12 - addedMonth;
+    yearResult--;
+  }
+
+  if (dateResult < 0) {
+    if (
+      monthResult == 1 ||
+      monthResult == 3 ||
+      monthResult == 5 ||
+      monthResult == 7 ||
+      monthResult == 8 ||
+      monthResult == 10 ||
+      monthResult == 12
+    ) {
+      dateResult = 31 + CurrentDate - AddedDate;
+      monthResult--;
+    } else if (monthResult == 2) {
+      dateResult = 28 + CurrentDate - AddedDate;
+      monthResult--;
+    } else if (
+      monthResult == 4 ||
+      monthResult == 6 ||
+      monthResult == 9 ||
+      monthResult == 11
+    ) {
+      dateResult = 30 + CurrentDate - AddedDate;
+      monthResult--;
+    }
+  }
+  DatePlace.innerHTML = dateResult;
+  MonthPlace.innerHTML = monthResult;
+  YearPlace.innerHTML = yearResult;
+}
